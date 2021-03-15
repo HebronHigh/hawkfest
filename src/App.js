d815521d44bf614a57113cb5d0ef50d21c873cba
Ethan Maher <mahere@go.lisd.net>
2021-03-15 13:40:57 -0500

    worked on getting colors done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import WorldMap from "./components/WorldMap";
+import data from './data/world.geo.json'
 import "./App.css";
 
 function App() {
+  const [property, setProperty] = useState("REGION")
   return (
     <React.Fragment>
-      <WorldMap />
+      <WorldMap data={ data } property={ property }/>
     </React.Fragment>
   );
 }
--- src/components/WorldMap.jsx
+++ src/components/WorldMap.jsx
@@ -1,57 +1,50 @@
-import React, { useRef, useEffect } from "react";
-import * as d3 from "d3";
-import * as topojson from 'topojson-client'
+import React, { useRef, useEffect, useState } from "react";
+import { select, geoPath, geoMercator, min, max, scaleLinear, color} from "d3";
 import useResizeObserver from "./useResizeObserver";
 
-function WorldMap() {
+/**
+ * Component that renders a map of Germany.
+ */
+
+function WorldMap({ data, property }) {
   const svgRef = useRef();
   const wrapperRef = useRef();
   const dimensions = useResizeObserver(wrapperRef);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  const colorArr = [color("red"), color("blue"), color("green"), color("yellow"), color("pink")]
 
   // will be called initially and on every data change
   useEffect(() => {
-    const svg = d3.select(svgRef.current);
+    const svg = select(svgRef.current);
+
+    const colorScale = (d) => {
+      return colorArr[d]
+    }
 
     // use resized dimensions
     // but fall back to getBoundingClientRect, if no dimensions yet.
     const { width, height } =
       dimensions || wrapperRef.current.getBoundingClientRect();
 
-    const t0 = {k:width/2/Math.PI,x:width/2,y:height/2};
-
     // projects geo-coordinates on a 2D plane
-    const projection = d3.geoMercator()
-    const path = d3.geoPath(projection)
+    const projection = geoMercator()
+      .fitSize([width, height], data)
+      .precision(100);
 
+    // takes geojson data,
+    // transforms that into the d attribute of a path element
+    const pathGenerator = geoPath(projection)
 
-    d3.json("https://d3js.org/world-110m.v1.json").then(function (world) {
-      let countries = topojson.feature(world, world.objects.countries).features;
-      let features = svg.selectAll("path")
-        .data(countries)
-        .enter()
-        .append("path")
-
-      let zoom = d3.zoom()
-        .on("zoom", function (event) {
-          let t = event.transform;
-          projection.scale(t.k).translate([t.x, t.y]);
-          features.attr("d", path)
-        })
-
-      svg.call(zoom);
-
-      svg.call(zoom.transform, d3.zoomIdentity.translate(width / 2, height / 2).scale(width / Math.PI / 2));
-
-      features.on("click", function (d) {
-        projection.fitSize([width, height], d)
-        let k = projection.scale() / t0.k
-        let x = projection.translate()[0] - t0.x * k
-        let y = projection.translate()[1] - t0.y * k
-        svg.call(zoom.transform, d3.zoomIdentity.translate(x, y).scale(k))
-      })
-    });
-  }, [dimensions]);
-
+    // render each country
+    svg
+      .selectAll(".country")
+      .data(data.features)
+      .join("path")
+      .attr("class", "country")
+      .attr("fill", feature => colorScale(feature.properties[property]))
+      .attr("d", feature => pathGenerator(feature));
+  }, [data, dimensions, property, selectedCountry]);
 
   return (
     <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
