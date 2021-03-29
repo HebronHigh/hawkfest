76337a205ed542010cbc89e13881ec2367cc8fb9
randompwner <ethanamaher@gmail.com>
2021-03-29 11:19:48 -0500

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom'
 import Map from './components/Map'
+import { useEffect } from 'react'
+import ReactGA from 'react-ga'
+import { createBrowserHistory } from 'history';
 
 function App() {
+  const history = createBrowserHistory();
+  history.listen(location => {
+    ReactGA.initialize('UA-193267831-1');
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  })
   return (
-    <BrowserRouter>
+    <BrowserRouter history={history}>
       <Map />
       <Route exact path='/hawkfest/pages/Lesotho/General%20Info.html' render={() => { window.location.href = "/hawkfest/pages/Lesotho/General%20Info.html" }} />
     </BrowserRouter>
