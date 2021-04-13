8cc05fa78cefd9bfc82f6bdf6a9efb718e1c32c8
randompwner <ethanamaher@gmail.com>
2021-04-12 21:12:37 -0500

    added hover info

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { MapContainer, GeoJSON, TileLayer } from 'react-leaflet'
 import data from './../data/countries.json'
 import 'leaflet/dist/leaflet.css'
 import './Map.css'
-import { latLng, latLngBounds } from 'leaflet';
+import L, { latLng, latLngBounds, layerGroup } from 'leaflet';
 
 /*
   REGION NUMBERS GUIDE:
@@ -13,38 +13,6 @@ import { latLng, latLngBounds } from 'leaflet';
   3: Europe & Russia
   4: Western Hemisphere
 */
-
-/**
- * Highlights a layer that was hovered over in the map
- *
- * @param {*} e the object that was hovered over
- *
- * @author Ethan Maher
- */
-function highlight(e) {
-  const layer = e.target;
-  layer.setStyle({
-    fillOpacity: e.target.feature.properties.HASDATA === 0 ? .9 : .7,
-    weight: 3,
-  })
-  layer.bringToFront();
-}
-
-/**
- * Unhighlights a layer that is no longer hovered over based on the region that layer is in (determined in JSON properties)
- *
- * @param {*} e the object that was stopped being hovered over
- *
- * @author Ethan Maher
- */
-function resetHighlight(e) {
-  const layer = e.target;
-  layer.setStyle({
-    weight: 1.5,
-    fillColor: getColors(layer.feature),
-    fillOpacity: e.target.feature.properties.HASDATA === 0 ? .8 : .5,
-  })
-}
 
 /**
  * Returns the correct color of a layer from the REGION property in the JSON
@@ -66,58 +34,84 @@ function getColors(feature) {
   }
 }
 
+function style(feature) {
+  return {
+    fillColor: getColors(feature),
+    weight: 1.5,
+    opacity: 1,
+    fillOpacity: feature.properties.HASDATA === 0 ? .8 : .5,
+    color: 'black'
+  };
+}
+
 /**
- * Class for Map component
+ * functional component for the map
  *
  * @author Ethan Maher
  */
-class Map extends Component {
-  onEachCountry = (country, layer) => {
+const Map = () => {
+  const [selected, setSelected] = useState({});
 
-    if (country.properties.HASDATA === 1) {
-      layer.bindPopup(function () {
-        const url = "/hawkfest/pages/" + country.properties.ADMIN + "/General%20Info.html"
-        return "<p>" + country.properties.ADMIN + "</p><a href=" + url + " id='viewmorebutton'>View Country Page</a>";
-      })
-    } else {
-      layer.bindPopup(function () {
-        return "<p>" + country.properties.ADMIN + "</p>";
-      })
+  function highlightFeature(e) {
+    let layer = e.target;
+    setSelected({
+      countryName: layer.feature.properties.ADMIN
+    })
+
+    layer.setStyle({
+      weight: 4,
+      fillOpacity: e.target.feature.properties.HASDATA === 0 ? .9 : .7,
+      color: 'gray',
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+      layer.bringToFront();
     }
+  }
+
+  function resetHighlight(e) {
+    setSelected({});
+    e.target.setStyle(style(e.target.feature));
+  }
+
+  function onEachCountry(country, layer) {
+    layer.bindPopup(function () {
+      const url = "/hawkfest/pages/" + country.properties.ADMIN + "/General%20Info.html"
+      return layer.feature.properties.HASDATA === 1 ? "<p>" + country.properties.ADMIN + "</p><a href=" + url + " id='viewmorebutton'>View Country Page</a>" : "<p>" + country.properties.ADMIN + "</p>";
+    });
 
     layer.on({
-      mouseover: highlight,
-      mouseout: resetHighlight
+      mouseover: highlightFeature,
+      mouseout: resetHighlight,
     })
   }
 
-  render() {
-    return (
-        <div>
-        <MapContainer style={{ height: "90vh" }} minZoom={2} maxZoom={7} zoomControl={false} zoom={2} center={[12.345, 12.345]} maxBounds={latLngBounds(latLng(-90,-200),latLng(90,200))} maxBoundsViscosity={1.0}>
-            <TileLayer
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
 
-            <GeoJSON
-              style={
-              function (feature) {
-                  return {
-                    fillColor: getColors(feature),
-                    weight: 1.5,
-                    fillOpacity: feature.properties.HASDATA === 0 ? .8 : .5,
-                    color: 'black',
-                  }
-                }
-              }
-              data={data.features}
-              onEachFeature={this.onEachCountry}
-            />
-          </MapContainer>
-        </div>
+  return (
+    <div>
+      {!selected.countryName && (
+          <div className="hover-info">Hover over an Area</div>
+        )}
+        {selected.countryName && (
+          <div className="info">
+            <strong>{selected.countryName}</strong>
+          </div>
+        )}
+      <MapContainer style={{ height: "90vh" }} minZoom={2} maxZoom={7} zoomControl={false} zoom={2} center={[12.345, 12.345]} maxBounds={latLngBounds(latLng(-90,-200),latLng(90,200))} maxBoundsViscosity={1.0}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <GeoJSON
+          style={style}
+          data={data.features}
+          onEachFeature={onEachCountry}
+        />
+      </MapContainer>
+    </div>
     )
-  }
+
 }
 
 export default Map
