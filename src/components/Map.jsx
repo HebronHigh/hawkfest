1e8ea8f6a2fe719d2b1bee155dde3aae496af64e
Ethan Maher <ethanamaher@gmail.com>
2021-04-17 22:46:20 -0500

    changed map colors

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -17,12 +17,18 @@ import L, { latLng, latLngBounds } from 'leaflet';
 /**
  * Color values for each region
  */
-const COLOR_0 = '#ffff00';
-const COLOR_1 = '#ff0000';
-const COLOR_2 = '#00ff00';
-const COLOR_3 = '#00ffff';
-const COLOR_4 = '#0000ff';
+const COLOR_0_FALSE = '#ffff00';
+const COLOR_1_FALSE = '#ff0000';
+const COLOR_2_FALSE = '#00ff00';
+const COLOR_3_FALSE = '#00ffff';
+const COLOR_4_FALSE = '#0000ff';
 const COLOR_DEFAULT = '#000000';
+
+const COLOR_0_TRUE = '#777700';
+const COLOR_1_TRUE = '#770000';
+const COLOR_2_TRUE = '#007700';
+const COLOR_3_TRUE = '#007777';
+const COLOR_4_TRUE = '#000077';
 
 /**
  * Returns the correct color of a layer from the REGION property in the JSON
@@ -35,11 +41,11 @@ const COLOR_DEFAULT = '#000000';
 function getColors(feature) {
   const regionNumber = feature.properties.REGION;
   switch (regionNumber) {
-    case 0: return COLOR_0;
-    case 1: return COLOR_1;
-    case 2: return COLOR_2;
-    case 3: return COLOR_3;
-    case 4: return COLOR_4;
+    case 0: return feature.properties.HASDATA === 1 ? COLOR_0_TRUE : COLOR_0_FALSE;
+    case 1: return feature.properties.HASDATA === 1 ? COLOR_1_TRUE : COLOR_1_FALSE;
+    case 2: return feature.properties.HASDATA === 1 ? COLOR_2_TRUE : COLOR_2_FALSE;
+    case 3: return feature.properties.HASDATA === 1 ? COLOR_3_TRUE : COLOR_3_FALSE;
+    case 4: return feature.properties.HASDATA === 1 ? COLOR_4_TRUE : COLOR_4_FALSE;
     default: return COLOR_DEFAULT;
   }
 }
@@ -55,7 +61,7 @@ function style(feature) {
     fillColor: getColors(feature),
     weight: 1.5,
     opacity: 1,
-    fillOpacity: feature.properties.HASDATA === 1 ? .8 : .4,
+    fillOpacity: feature.properties.HASDATA === 1 ? .7 : .4,
     color: 'black'
   };
 }
@@ -83,7 +89,7 @@ const Map = () => {
 
     layer.setStyle({
       weight: 4,
-      fillOpacity: e.target.feature.properties.HASDATA === 1 ? 1 : .6,
+      fillOpacity: e.target.feature.properties.HASDATA === 1 ? .9 : .6,
       color: 'gray',
     });
 
@@ -110,7 +116,14 @@ const Map = () => {
   function onEachCountry(country, layer) {
     layer.bindPopup(function () {
       let countryName = country.properties.ADMIN;
-      if(countryName === 'United States of America') countryName = 'United States'
+
+      /*
+        Have to do this for countries whose ADMIN is different than their page name
+      */
+      if (countryName === 'United States of America') countryName = 'United States'
+
+
+
       const url = "/hawkfest/pages/" + countryName.replaceAll(" ", "%20") + "/General%20Info.html"
       return country.properties.HASDATA === 1
       ? "<div>" + country.properties.ADMIN + "</div><a href=" + url + " id='viewmorebutton'>View Country Page</a>"
@@ -135,11 +148,11 @@ const Map = () => {
       )}
       <div className="legend">
           <p>Please Click on a Country With a Darker Color to Get Started</p>
-          <div style={{ "--color": COLOR_0 }}>Southwest Asia & Northern Africa</div>
-          <div style={{ "--color": COLOR_1 }}>Sub-Saharan Africa & Australia</div>
-          <div style={{ "--color": COLOR_2 }}>East & Southeast Asia</div>
-          <div style={{ "--color": COLOR_3 }}>Europe & Russia</div>
-          <div style={{ "--color": COLOR_4 }}>Western Hemisphere</div>
+          <div style={{ "--color": COLOR_0_FALSE }}>Southwest Asia & Northern Africa</div>
+          <div style={{ "--color": COLOR_1_FALSE }}>Sub-Saharan Africa & Australia</div>
+          <div style={{ "--color": COLOR_2_FALSE }}>East & Southeast Asia</div>
+          <div style={{ "--color": COLOR_3_FALSE }}>Europe & Russia</div>
+          <div style={{ "--color": COLOR_4_FALSE }}>Western Hemisphere</div>
         </div>
       <MapContainer style={{ height: "90vh" }} minZoom={2} maxZoom={7} zoomControl={false} zoom={2} center={[12.345, 12.345]} maxBounds={latLngBounds(latLng(-90,-200),latLng(90,200))} maxBoundsViscosity={1.0}>
         <TileLayer
