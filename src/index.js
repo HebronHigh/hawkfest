c0204aad76d36290be4382b0c4ba05d5271a1686
Ethan Maher <ethanamaher@gmail.com>
2021-04-18 20:18:00 -0500

    removed google analytics

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Analytics from 'react-router-ga';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -8,10 +7,7 @@ import './fonts/leaves_and_ground.ttf'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Analytics id='UA-193267831-1' debug>
-      <App />
-    </Analytics>
-
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
